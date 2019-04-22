98c005c51a7afe0a278cac892f5074a8f31ce016
feryae <peryardiansyah@gmail.com>
2019-04-23 05:54:51 +0700

    Commit

---
+++ application/views/bookpage.php
@@ -0,0 +1,59 @@
+<div class="inner-container" style="padding-top:10%;;">
+  <div style="display:flex;">
+    <div class="">
+      <div class="">
+        <img src="" style="width:200px; height:300px;">
+      </div>
+      <div style="display:flex;">
+        <p style="padding-right:5px;">Bagikan : </p>
+        <a href="#"><img src="https://www.gramedia.com/assets/social-icon/facebook-9.svg" alt=""></a>
+        <a href="#"><img   src="https://www.gramedia.com/assets/social-icon/twitter-5.svg" alt=""></a>
+      </div>
+    </div>
+    <div class="" style="padding:0px 6% 0;">
+      <h2>Nicola Yoon: Sun Is Also A Star</h2>
+      <h3>Random House Us</h3>
+    </div>
+    <div style="width:262px">
+      <div class="" style="border:1px solid black; width:100%; height:102px; ">
+        <div class="">
+          <p>Soft Cover</p>
+        </div>
+        <div class="">
+          <p>Rp.70000</p>
+        </div>
+      </div>
+      <div class="" align="center">
+        <div class="" style="padding:5% 0 5%;">
+          <button type="button" name="button" style="width:100%; height:40px; border-radius:4px;">Beli Sekarang</button>
+        </div>
+        <div class="">
+          <button type="button" name="button" style="width:100%; height:40px; border-radius:4px;">Tambah Wishlist</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+  		<ul class="nav nav-tabs">
+  			<li><a data-toggle="tab" href="#bookdescription">Deskripsi</a></li>
+  			<li><a data-toggle="tab" href="#bookdetail">Detail</a></li>
+  		</ul>
+  		<div class="tab-content">
+  			<div id="bookdescription" class="tab-pane fade in active">
+          <p>Kami adalah keterangan</p>
+  			</div>
+  			<div id="bookdetail" class="tab-pane fade">
+          <div style="display:flex;">
+            <div class="">
+
+            </div>
+            <div class="">
+
+            </div>
+
+          </div>
+  			</div>
+  		</div>
+  	</div>
+
+</div>
