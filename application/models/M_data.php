b21aef89c6d817012c579c8986c610c0ff25e2a0
putuwinda <windaardiyanti@student.telkomuniversity.ac.id>
2019-05-04 20:33:08 +0700

    ..

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -23,15 +23,6 @@ class M_data extends CI_Model{
 	function get_pengguna(){
 		return $this->db->get('pengguna');
 	}
-<<<<<<< HEAD
-
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> c9e8647ba2b04aa7d5d357d7741a4fde5a7076a3
->>>>>>> a2706a53d8ecff072a4388db5bd5e4b4dfa052e5
 
 	function detil_pengguna($email){
 		return $this->db->query("SELECT * FROM pengguna where email = '$email'");
@@ -54,36 +45,6 @@ class M_data extends CI_Model{
 		return $this->db->empty_table('orderan');
 	}
 
-<<<<<<< HEAD
-	function get_join($from,$join,$where){
-		$this->db->select('*');
-		$this->db->from($from);
-		$this->db->join($join, $where);
-		return $this->db->get()->result();
-	}
-
-	function get_by_id($id,$col,$tabname){
-		$this->db->where($col,$id);
-		$data = $this->db->get($tabname);
-		return $data->row_array();
-	}
-	function search_by_id($query,$filter,$tabname){
-		$this->db->select('*');
-		$this->db->from($tabname);
-		$this->db->like($filter, $query);
-		return $this->db->get()->row_array();
-=======
-	public function update_pengguna($table,$data_update){
-
-		$this->db->where('email', $this->session->userdata('email'));
-
- 	 $update = $this->db->update($table,$data_update);
-
-
->>>>>>> c9e8647ba2b04aa7d5d357d7741a4fde5a7076a3
-	}
-
-
 	function get_join($from,$join,$where){
 		$this->db->select('*');
 		$this->db->from($from);
@@ -102,6 +63,26 @@ class M_data extends CI_Model{
 		$this->db->like($filter, $query);
 		return $this->db->get()->row_array();
 	}
+
+	//
+	// function get_join($from,$join,$where){
+	// 	$this->db->select('*');
+	// 	$this->db->from($from);
+	// 	$this->db->join($join, $where);
+	// 	return $this->db->get()->result();
+	// }
+
+	// function get_by_id($id,$col,$tabname){
+	// 	$this->db->where($col,$id);
+	// 	$data = $this->db->get($tabname);
+	// 	return $data->row_array();
+	// }
+	// function search_by_id($query,$filter,$tabname){
+	// 	$this->db->select('*');
+	// 	$this->db->from($tabname);
+	// 	$this->db->like($filter, $query);
+	// 	return $this->db->get()->row_array();
+	// }
 	public function update_pengguna($table,$data_update){
 
 		$this->db->where('email', $this->session->userdata('email'));
